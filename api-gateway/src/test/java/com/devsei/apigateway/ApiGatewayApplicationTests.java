1bc0d33def41f2090e92bdcef56ee86c58db2235
Sei Kim <workju1124@gmail.com>
2024-01-22 15:38:03 +0900

    feat: apigateway 생성

---
+++ api-gateway/src/test/java/com/devsei/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsei.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
